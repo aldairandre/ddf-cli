524b0a899cc1df15b9418920d10cc91ccae2fc0b
aldairandre <aldairandre99@gmail.com>
2022-11-16 18:33:08 +0000

    ♻️ Refactor Code
    Deleting test fields and using eslint rules

--- src/utils/cli.js
+++ src/utils/cli.js
@@ -1,46 +1,4 @@
 const meow = require("meow");
-const meowHelp = require("cli-meow-help");
-
-const flags = {
-	clear: {
-		type: `boolean`,
-		default: true,
-		alias: `c`,
-		desc: `Clear the console`
-	},
-	noClear: {
-		type: `boolean`,
-		default: false,
-		desc: `Don't clear the console`
-	},
-	debug: {
-		type: `boolean`,
-		default: false,
-		alias: `d`,
-		desc: `Print debug info`
-	},
-	version: {
-		type: `boolean`,
-		alias: `v`,
-		desc: `Print CLI version`
-	}
-};
-
-const commands = {
-	help: { desc: `Print help info` }
-};
-
-const helpText = meowHelp({
-	name: `ddf`,
-	flags,
-	commands
-});
-
-const options = {
-	inferType: true,
-	description: false,
-	hardRejection: false,
-	flags
-};
+const { helpText, options } = require("../constants-utils/cliConst");
 
 module.exports = meow(helpText, options);
